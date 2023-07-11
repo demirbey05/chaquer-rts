cacfe3309dd2c7fd8f71da71112e0b2c794a410d
İsmail Altay <ismailaltay3553@gmail.com>
2023-07-11 19:31:27 +0300

    War result betwwen armies are implemented. Both side of the war will get result at the same time when the war is resulted.

--- packages/client/src/components/WarResultComp/WarResultComp.tsx
+++ packages/client/src/components/WarResultComp/WarResultComp.tsx
@@ -1,11 +1,24 @@
+import { usePlayer } from '../../context/PlayerContext';
 import { useScoreTable } from '../../hooks/useScoreTable'
 
 function WarResultComp() {
     const warResults = useScoreTable(5);
+    const { userWallet } = usePlayer();
 
     return (
         <div className="war-result-bar">
-
+            <h4 className='text-white text-center mt-2 mb-2 border-bottom-1'>War Results</h4>
+            {warResults && warResults.data.map((data, key) => {
+                if (data?.isDraw === true) {
+                    return <p className='text-white p-4 bg-primary' key={key}>No body has won the war. Draw!</p>
+                }
+                else if (data?.winner === userWallet?.address.toLocaleLowerCase()) {
+                    return <p className='text-white p-4 bg-success' key={key}>You have won the war. Win!</p>
+                }
+                else if (data?.loser === userWallet?.address.toLocaleLowerCase()) {
+                    return <p className='text-white p-4 bg-danger' key={key}>You have lost the war. Lose!</p>
+                }
+            })}
         </div>
     )
 }
