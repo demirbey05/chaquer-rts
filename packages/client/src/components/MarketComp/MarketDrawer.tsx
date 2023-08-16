a5384f1af7803b9af0b48b7af523c15c9ba64579
İsmail Altay <ismailaltay3553@gmail.com>
2023-08-16 17:30:16 +0300

    AutoResourceSell Buttons are added to MarketDrawer.tsx . Now, players can sell resource number of 30-100-500 with a button click.

--- packages/client/src/components/MarketComp/MarketDrawer.tsx
+++ packages/client/src/components/MarketComp/MarketDrawer.tsx
@@ -7,7 +7,7 @@ import { usePlayer } from '../../context/PlayerContext';
 
 export const MarketDrawer = () => {
     const { userWallet } = usePlayer();
-    const { systemCalls }: any = useMUD();
+    const { systemCalls } = useMUD();
 
     const [numWood, setNumWood] = useState<string>("");
     const [numFood, setNumFood] = useState<string>("");
@@ -16,6 +16,18 @@ export const MarketDrawer = () => {
     const [isWoodDisabled, setIsWoodDisabled] = useState<boolean>(true);
     const [isFoodDisabled, setIsFoodDisabled] = useState<boolean>(true);
     const [isGoldDisabled, setIsGoldDisabled] = useState<boolean>(true);
+
+    const [is100WoodDisabled, setIs100WoodDisabled] = useState<boolean>(true);
+    const [is100FoodDisabled, setIs100FoodDisabled] = useState<boolean>(true);
+    const [is100GoldDisabled, setIs100GoldDisabled] = useState<boolean>(true);
+
+    const [is30WoodDisabled, setIs30WoodDisabled] = useState<boolean>(true);
+    const [is30FoodDisabled, setIs30FoodDisabled] = useState<boolean>(true);
+    const [is30GoldDisabled, setIs30GoldDisabled] = useState<boolean>(true);
+
+    const [is500WoodDisabled, setIs500WoodDisabled] = useState<boolean>(true);
+    const [is500FoodDisabled, setIs500FoodDisabled] = useState<boolean>(true);
+    const [is500GoldDisabled, setIs500GoldDisabled] = useState<boolean>(true);
 
     const numberOfResource: any = useNumberOfResource(userWallet!.address, 1)?.value;
 
@@ -42,6 +54,75 @@ export const MarketDrawer = () => {
             document.removeEventListener('keydown', handleKeyPress);
         };
     }, []);
+
+    useEffect(() => {
+        // Food
+        if (numberOfResource && numberOfResource!.numOfFood < 30) {
+            setIs30FoodDisabled(true);
+        }
+        else {
+            setIs30FoodDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfFood < 100) {
+            setIs100FoodDisabled(true);
+        }
+        else {
+            setIs100FoodDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfFood < 500) {
+            setIs500FoodDisabled(true);
+        }
+        else {
+            setIs500FoodDisabled(false);
+        }
+
+        // Wood
+        if (numberOfResource && numberOfResource!.numOfWood < 30) {
+            setIs30WoodDisabled(true);
+        }
+        else {
+            setIs30WoodDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfWood < 100) {
+            setIs100WoodDisabled(true);
+        }
+        else {
+            setIs100WoodDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfWood < 500) {
+            setIs500WoodDisabled(true);
+        }
+        else {
+            setIs500WoodDisabled(false);
+        }
+
+        // Gold
+        if (numberOfResource && numberOfResource!.numOfGold < 30) {
+            setIs30GoldDisabled(true);
+        }
+        else {
+            setIs30GoldDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfGold < 100) {
+            setIs100GoldDisabled(true);
+        }
+        else {
+            setIs100GoldDisabled(false);
+        }
+
+        if (numberOfResource && numberOfResource!.numOfGold < 500) {
+            setIs500GoldDisabled(true);
+        }
+        else {
+            setIs500GoldDisabled(false);
+        }
+
+    }, [numberOfResource])
 
     useEffect(() => {
         if (numFood && numFood.toString().length === 0) {
@@ -109,6 +190,43 @@ export const MarketDrawer = () => {
         }
     }
 
+    const handle30ResourceSell = async ({ resourceType }: any) => {
+        if (resourceType !== undefined) {
+            console.log("resource type var")
+            const tx = await systemCalls.sellResource(1, 30, resourceType);
+            if (tx) {
+                console.log("Success sell during 30 resource sell.")
+            }
+            else {
+                console.log("Error occured during 30 resource sell.")
+            }
+        }
+    }
+
+    const handle100ResourceSell = async ({ resourceType }: any) => {
+        if (resourceType !== undefined) {
+            const tx = await systemCalls.sellResource(1, 100, resourceType);
+            if (tx) {
+                console.log("Success sell during 100 resource sell.")
+            }
+            else {
+                console.log("Error occured during 100 resource sell.")
+            }
+        }
+    }
+
+    const handle500ResourceSell = async ({ resourceType }: any) => {
+        if (resourceType !== undefined) {
+            const tx = await systemCalls.sellResource(1, 500, resourceType);
+            if (tx) {
+                console.log("Success sell during 500 resource sell.")
+            }
+            else {
+                console.log("Error occured during 500 resource sell.")
+            }
+        }
+    }
+
     const marketDrawerButtonStyles: any = {
         zIndex: 1,
         height: "60px",
@@ -122,7 +240,8 @@ export const MarketDrawer = () => {
     }
 
     const marketDrawerDivStyles = {
-        height: "325px",
+        height: "500px",
+        width: "425px",
         marginTop: "90px",
         padding: "10px"
     }
@@ -154,9 +273,42 @@ export const MarketDrawer = () => {
                         <h4 className="border-bottom text-center p-2 font-bold">Sell Resource</h4>
                     </Tooltip>
                     <div className="d-flex align-middle justify-center mt-2 mb-2">
-                        <ResourceCard resourceEmoji={"🌽"} resourceName={"Food"} setResourceCount={setNumFood} isDisabled={isFoodDisabled} handleSell={handleFoodSell} />
-                        <ResourceCard resourceEmoji={"🪓"} resourceName={"Wood"} setResourceCount={setNumWood} isDisabled={isWoodDisabled} handleSell={handleWoodSell} />
-                        <ResourceCard resourceEmoji={"⛏️"} resourceName={"Gold"} setResourceCount={setNumGold} isDisabled={isGoldDisabled} handleSell={handleGoldSell} />
+                        <ResourceCard resourceEmoji={"🌽"}
+                            resourceName={"Food"}
+                            setResourceCount={setNumFood}
+                            resourceType={0}
+                            isDisabled={isFoodDisabled}
+                            is30Disabled={is30FoodDisabled}
+                            is100Disabled={is100FoodDisabled}
+                            is500Disabled={is500FoodDisabled}
+                            handle30ResourceSell={handle30ResourceSell}
+                            handle100ResourceSell={handle100ResourceSell}
+                            handle500ResourceSell={handle500ResourceSell}
+                            handleSell={handleFoodSell} />
+                        <ResourceCard resourceEmoji={"🪓"}
+                            resourceName={"Wood"}
+                            setResourceCount={setNumWood}
+                            resourceType={1}
+                            isDisabled={isWoodDisabled}
+                            is30Disabled={is30WoodDisabled}
+                            is100Disabled={is100WoodDisabled}
+                            is500Disabled={is500WoodDisabled}
+                            handle30ResourceSell={handle30ResourceSell}
+                            handle100ResourceSell={handle100ResourceSell}
+                            handle500ResourceSell={handle500ResourceSell}
+                            handleSell={handleWoodSell} />
+                        <ResourceCard resourceEmoji={"⛏️"}
+                            resourceName={"Gold"}
+                            setResourceCount={setNumGold}
+                            resourceType={2}
+                            isDisabled={isGoldDisabled}
+                            is30Disabled={is30GoldDisabled}
+                            is100Disabled={is100GoldDisabled}
+                            is500Disabled={is500GoldDisabled}
+                            handle30ResourceSell={handle30ResourceSell}
+                            handle100ResourceSell={handle100ResourceSell}
+                            handle500ResourceSell={handle500ResourceSell}
+                            handleSell={handleGoldSell} />
                     </div>
                 </div>
             </div>
@@ -169,6 +321,13 @@ interface ResourceCardPropTypes {
     resourceName: string,
     setResourceCount: React.Dispatch<React.SetStateAction<string>>,
     isDisabled: boolean,
+    is100Disabled: boolean,
+    is30Disabled: boolean,
+    is500Disabled: boolean,
+    handle30ResourceSell: any,
+    handle100ResourceSell: any,
+    handle500ResourceSell: any,
+    resourceType: number,
     handleSell: any,
 }
 
@@ -183,13 +342,16 @@ const ResourceCard = (props: ResourceCardPropTypes) => {
             </div>
             <div className="row justify-content-center mt-2 w-100">
                 <input
-                    className="form-control w-100"
+                    className="form-control"
                     type="number"
                     id={props.resourceName}
                     onChange={(e: any) => props.setResourceCount(e.target.value)}
                     onClick={(e: any) => e.target.select()} />
             </div>
-            <SellButton isDisabled={props.isDisabled} handleSell={props.handleSell} resourceName={props.resourceName} />
+            <SellButton isDisabled={props.isDisabled} handleSell={props.handleSell} numOfResource={""} resourceEmoji={props.resourceEmoji} />
+            <AutoSellButton isDisabled={props.is30Disabled} resourceType={props.resourceType} handleSell={props.handle30ResourceSell} numOfResource={"30"} resourceEmoji={props.resourceEmoji} />
+            <AutoSellButton isDisabled={props.is100Disabled} resourceType={props.resourceType} handleSell={props.handle100ResourceSell} numOfResource={"100"} resourceEmoji={props.resourceEmoji} />
+            <AutoSellButton isDisabled={props.is500Disabled} resourceType={props.resourceType} handleSell={props.handle500ResourceSell} numOfResource={"500"} resourceEmoji={props.resourceEmoji} />
         </div>
     )
 }
@@ -197,7 +359,8 @@ const ResourceCard = (props: ResourceCardPropTypes) => {
 interface SellButtonPropTypes {
     isDisabled: boolean,
     handleSell: any,
-    resourceName: string
+    resourceEmoji: string,
+    numOfResource: string
 }
 
 const SellButton = (props: SellButtonPropTypes) => {
@@ -205,13 +368,39 @@ const SellButton = (props: SellButtonPropTypes) => {
         <Button
             colorScheme="whatsapp"
             border="solid"
+            fontSize="13px"
             textColor="dark"
             className="w-75"
             mt={4}
             isDisabled={props.isDisabled}
             onClick={() => props.handleSell()}
         >
-            Sell {props.resourceName}
+            Sell {props.numOfResource} {props.resourceEmoji}
+        </Button>
+    )
+}
+
+interface AutoSellButtonPropTypes {
+    isDisabled: boolean,
+    handleSell: any,
+    resourceEmoji: string,
+    numOfResource: string,
+    resourceType: number
+}
+
+const AutoSellButton = (props: AutoSellButtonPropTypes) => {
+    return (
+        <Button
+            colorScheme="orange"
+            border="solid"
+            fontSize="13px"
+            textColor="dark"
+            className="w-75"
+            mt={4}
+            isDisabled={props.isDisabled}
+            onClick={() => props.handleSell({ resourceType: props.resourceType })}
+        >
+            Sell {props.numOfResource} {props.resourceEmoji}
         </Button>
     )
 }
