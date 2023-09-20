f1a252955f0f5388f7d1b5a4fb37b8a78f82a751
İsmail Altay <ismailaltay3553@gmail.com>
2023-09-20 11:16:14 +0300

    Tailwindcss production cli implemented. CDN is removed.

--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
+    "./src/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
