d7dd39468f959110796bfd4f4375a3716614c533
Okan <huseyinokand@gmail.com>
2023-09-25 14:25:50 +0300

    distance parameter is changed

--- packages/contracts/src/systems/ArmyUpdateSystem.sol
+++ packages/contracts/src/systems/ArmyUpdateSystem.sol
@@ -80,7 +80,7 @@ contract ArmyUpdateSystem is System {
       (uint32 xOne, uint32 yOne, ) = Position.get(armyOneID);
       (uint32 xTwo, uint32 yTwo, ) = Position.get(armyTwoID);
       uint32 distanceBetween = LibMath.manhattan(xOne, yOne, xTwo, yTwo);
-      if (distanceBetween > 1) {
+      if (distanceBetween > 2) {
         revert ArmyUpdate__TooFar();
       }
     }
